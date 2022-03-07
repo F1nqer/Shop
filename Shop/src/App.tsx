fa5edc073f5c17ecc7a3c88dd028d99dc69216e0
a.kulprisov <arthur.kulpeisov@gmail.com>
2022-03-07 17:24:59 +0600

    SomeFixes

--- Shop/src/App.tsx
+++ Shop/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
                   Edit <code>src/App.tsx</code> and save to reload.
-                  <h1>kotak</h1>
+                  <h1>Kaspi</h1>
         </p>
         <a
           className="App-link"
